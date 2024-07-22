8eda09ed97266b569e6ed36a74dd05f20b768f99
Justin Ewelike <54476858+jayte054@users.noreply.github.com>
2024-07-22 15:17:54 +0100

    update on projects

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -2,71 +2,160 @@ import "./project.css";
 import smoozepic from "../../images/Screenshot 2023-08-03 at 18.41.02.png"
 import foodpic from "../../images/Screenshot 2023-08-03 at 18.48.08.png"
 import shoppingpic from "../../images/Screenshot (15).png"
+import blogpic from "../../images/blog_image.png";
 
 export const Project = () => {
 
     return (
-        <div>
-            <div className="project-container">
-                <div className="project-names">                
-            <h1 style={{textAlign: "center"}}>My Projects</h1>
+      <div>
+        <div className="project-container">
+          <div className="project-names">
+            <h1 style={{ textAlign: "center" }}>My Projects</h1>
             <ul>
-                <div className="shopping-container">
+              <div className="shopping-container">
                 <div className="shopping">
-                <li>Shopping Manager</li>
-                    <p>The shopping manager app was built with the idea of improving the productivity of stores to meet with 
-                        the needs of customers who have busy schedules to keep to. 
-                    </p>
-                    <span> <strong style={{paddingTop:"0.1rem"}}>Technologies used include;</strong> </span>
-                        <p>NestJs, ReactJs, Postgres, </p>
-                        <span> <strong style={{paddingTop:"0.1rem"}}>Status; </strong> 
-                                     Deployed ➡️ <a href="https://shoppingmanager.vercel.app" target= "_blank" rel="noreferrer">Shopping manager</a>
-                        </span>
+                  <li>AI Analytics  Blog Backend project </li>
+                  <p>
+                    The blog project was built to enable ease of 
+                    use for clients nd ease of oversight from the admin
+                    with the necessary authorizations were needed.
+                  </p>
+                  <span>
+                    {" "}
+                    <strong style={{ paddingTop: "0.1rem" }}>
+                      Technologies used include;
+                    </strong>{" "}
+                  </span>
+                  <p>NestJs, Postgres, </p>
+                  <span>
+                    {" "}
+                    <strong style={{ paddingTop: "0.1rem" }}>Status; </strong>
+                    Deployed ➡️{" "}
+                    <a
+                      href= "https://ai-blog-assessment-1.onrender.com/api"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      AI Analytics Blog Backend Poject
+                    </a>
+                  </span>
                 </div>
                 <div className="shopping-img">
-                <span><img src= {shoppingpic} alt="shopping manager pic" /></span>
+                  <span>
+                    <img src={blogpic} alt="AI Blog pic" />
+                  </span>
                 </div>
-                </div>   
-                    <div className="smooze-container">
-                        <div className="smooze">
-                             <li>Smooze App</li>
-                                <p>The Smooze App project was developed to help cushion the effect of the stressful environemts, providing an escape
-                                    through a platform to play your favorite songs and podcasts
-                                </p>
-                                <span> <strong style={{paddingTop:"0.1rem"}}>Technologies used include;</strong> </span>
-                                        <p>ExpressJs, ReactJs, Postgres, </p>
-                                <span> <strong style={{paddingTop:"0.1rem"}}>Status; </strong> 
-                                     Deployed ➡️ <a href="https://smoozepro.netlify.app" target= "_blank" rel="noreferrer">Smooze pro</a>
-                                </span>
-                        </div>
-                        <div>
-                        <span className="smooze-img"><img src = {smoozepic} alt="smooze pic" /></span>
-                        </div>
+              </div>
+              <div className="shopping-container">
+                <div className="shopping">
+                  <li>Shopping Manager</li>
+                  <p>
+                    The shopping manager app was built with the idea of
+                    improving the productivity of stores to meet with the needs
+                    of customers who have busy schedules to keep to.
+                  </p>
+                  <span>
+                    {" "}
+                    <strong style={{ paddingTop: "0.1rem" }}>
+                      Technologies used include;
+                    </strong>{" "}
+                  </span>
+                  <p>NestJs, ReactJs, Postgres, </p>
+                  <span>
+                    {" "}
+                    <strong style={{ paddingTop: "0.1rem" }}>Status; </strong>
+                    Deployed ➡️{" "}
+                    <a
+                      href="https://shoppingmanager.vercel.app"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Shopping manager
+                    </a>
+                  </span>
                 </div>
-                <div className="food-container">
-                    <div className="food">
-                        <li>Food App</li>
-                        <p>The food app was developed to enable vendors and customers engage seemlessly with a functional payment
-                            platform integrated.
-                        </p>
-                        <span> <strong style={{paddingTop:"0.1rem"}}>Technologies used include;</strong> </span>
-                            <p>ExpressJs, ReactJs, Mongodb, </p>
-                        <span> <strong style={{paddingTop:"0.1rem"}}>Status; </strong> 
-                        Deployed ➡️ <a href="https://foodorderring.netlify.app" target= "_blank" rel="noreferrer">Food App</a>
-                        </span>
-                    </div>
-                    <div>
-                        <span className="food-img"><img src={foodpic} alt="food pic" /></span>
-                    </div>
+                <div className="shopping-img">
+                  <span>
+                    <img src={shoppingpic} alt="shopping manager pic" />
+                  </span>
                 </div>
+              </div>
+              <div className="smooze-container">
+                <div className="smooze">
+                  <li>Smooze App</li>
+                  <p>
+                    The Smooze App project was developed to help cushion the
+                    effect of the stressful environemts, providing an escape
+                    through a platform to play your favorite songs and podcasts
+                  </p>
+                  <span>
+                    {" "}
+                    <strong style={{ paddingTop: "0.1rem" }}>
+                      Technologies used include;
+                    </strong>{" "}
+                  </span>
+                  <p>ExpressJs, ReactJs, Postgres, </p>
+                  <span>
+                    {" "}
+                    <strong style={{ paddingTop: "0.1rem" }}>Status; </strong>
+                    Deployed ➡️{" "}
+                    <a
+                      href="https://smoozepro.netlify.app"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Smooze pro
+                    </a>
+                  </span>
+                </div>
+                <div>
+                  <span className="smooze-img">
+                    <img src={smoozepic} alt="smooze pic" />
+                  </span>
+                </div>
+              </div>
+              <div className="food-container">
+                <div className="food">
+                  <li>Food App</li>
+                  <p>
+                    The food app was developed to enable vendors and customers
+                    engage seemlessly with a functional payment platform
+                    integrated.
+                  </p>
+                  <span>
+                    {" "}
+                    <strong style={{ paddingTop: "0.1rem" }}>
+                      Technologies used include;
+                    </strong>{" "}
+                  </span>
+                  <p>ExpressJs, ReactJs, Mongodb, </p>
+                  <span>
+                    {" "}
+                    <strong style={{ paddingTop: "0.1rem" }}>Status; </strong>
+                    Deployed ➡️{" "}
+                    <a
+                      href="https://foodorderring.netlify.app"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Food App
+                    </a>
+                  </span>
+                </div>
+                <div>
+                  <span className="food-img">
+                    <img src={foodpic} alt="food pic" />
+                  </span>
+                </div>
+              </div>
             </ul>
-            </div>
-            <div className="project-image">
-                {/* <span><img src="my-app/src/images/Screenshot 2023-08-03 at 18.41.02.png" alt="shopping manager pic" /></span>
+          </div>
+          <div className="project-image">
+            {/* <span><img src="my-app/src/images/Screenshot 2023-08-03 at 18.41.02.png" alt="shopping manager pic" /></span>
                 <span><img src = {smoozepic} alt="shopping manager pic" /></span>
                 <span><img src={foodpic} alt="shopping manager pic" /></span> */}
-            </div>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
